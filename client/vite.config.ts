1b2e4ee22adb8ac77d3f6301ef4197a798846411
leehojun0314 <dlghwns0314@naver.com>
2024-10-21 10:13:16 +1000

    change vite config port to 8080

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': '/src',
-		},
-	},
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  preview: {
+    port: 8080,
+  },
 });
